3cc26506e543b8fecafe1d7e91512e6eb763f0aa
Mohsen <mohsen@germanautolabs.com>
2017-11-04 11:07:38 +0100

    Add AdMob to Play flavor

--- app/src/main/java/com/tehran/traffic/AnalyticsApplication.java
+++ app/src/main/java/com/tehran/traffic/AnalyticsApplication.java
@@ -6,25 +6,19 @@ package com.tehran.traffic;
 
 import android.app.Application;
 
-
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-import org.acra.sender.HttpSender;
-
-@ReportsCrashes(
-        reportType = HttpSender.Type.JSON,
-        httpMethod = HttpSender.Method.PUT,
-        formUri = "https://collector.tracepot.com/1bb22253",
-        mode = ReportingInteractionMode.DIALOG,
-        resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
-        resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
-        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user text field input with this text resource as a label
-        resDialogEmailPrompt = R.string.crash_user_email_label, // optional. When defined, adds a user email text entry with this text resource as label. The email address will be populated from SharedPreferences and will be provided as an ACRA field if configured.
-        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
-)
+//@ReportsCrashes(
+//        reportType = HttpSender.Type.JSON,
+//        httpMethod = HttpSender.Method.PUT,
+//        formUri = "https://collector.tracepot.com/1bb22253",
+//        mode = ReportingInteractionMode.DIALOG,
+//        resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+//        resDialogText = R.string.crash_dialog_text,
+//        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+//        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+//        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user text field input with this text resource as a label
+//        resDialogEmailPrompt = R.string.crash_user_email_label, // optional. When defined, adds a user email text entry with this text resource as label. The email address will be populated from SharedPreferences and will be provided as an ACRA field if configured.
+//        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+//)
 public class AnalyticsApplication extends Application {
     //Logging TAG
     private static final String TAG = "AnalyticsApplication";
@@ -38,7 +32,7 @@ public class AnalyticsApplication extends Application {
         super.onCreate();
 
         // The following line triggers the initialization of ACRA
-        ACRA.init(this);
+//        ACRA.init(this);
 
     }
 
