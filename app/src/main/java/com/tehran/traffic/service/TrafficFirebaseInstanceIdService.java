5ce091826d1287d710ad67d681c9b711c56d6b8d
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2023-03-26 14:33:28 +0200

    Migrate to Gradle KTS

--- app/src/main/java/com/tehran/traffic/service/TrafficFirebaseInstanceIdService.java
+++ app/src/main/java/com/tehran/traffic/service/TrafficFirebaseInstanceIdService.java
@@ -5,7 +5,7 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.mirhoseini.appsettings.AppSettings;
+import com.mohsenoid.appsettings.AppSettings;
 
 /**
  * Created by Mohsen on 04/12/2016.
